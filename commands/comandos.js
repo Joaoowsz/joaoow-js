787a3c5190a5503a023b69743d16cc278fedea49
João Catozzi <jao_catozzi@hotmail.com>
2018-07-13 16:38:26 -0300

    a

--- commands/comandos.js
+++ commands/comandos.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
 
     if (mention == null) { return; }
     message.delete();
-    mentionMessage = message.send("Lista de comandos: \n!addrole <@username> <cargo> - Setar o cargo de um membro.\n!removerole <@username> <cargo> - Remover o cargo de um membro\n!anunciar <msg> - Enviar um anúncio.\n!avatar <@username> - Ver o avatar de um membro.\n!clear <quantidade> - Limpar certa quantidade de mensagens.\n!botinfo - Veja as informações do bot.\n!dog - Ver um cachorro fofo ;3\n!info - Veja suas informações.\n!kick <@username> <motivo> - Kickar um membro do discord.\n!ban <@username> <motivo> - Banir um membro do discord.\n!report - Reportar um membro do discord.\n!serverinfo - Veja as informações do servidor.\n!warn <@username> <motivo> - Dar um aviso a um membro.\n!warnlevel <@username> - Veja a quantidade de warns de um membro");
+    mentionMessage = message.content("Lista de comandos: \n!addrole <@username> <cargo> - Setar o cargo de um membro.\n!removerole <@username> <cargo> - Remover o cargo de um membro\n!anunciar <msg> - Enviar um anúncio.\n!avatar <@username> - Ver o avatar de um membro.\n!clear <quantidade> - Limpar certa quantidade de mensagens.\n!botinfo - Veja as informações do bot.\n!dog - Ver um cachorro fofo ;3\n!info - Veja suas informações.\n!kick <@username> <motivo> - Kickar um membro do discord.\n!ban <@username> <motivo> - Banir um membro do discord.\n!report - Reportar um membro do discord.\n!serverinfo - Veja as informações do servidor.\n!warn <@username> <motivo> - Dar um aviso a um membro.\n!warnlevel <@username> - Veja a quantidade de warns de um membro");
     mention.sendMessage(mentionMessage);
     message.reply("enviei todos os meus comandos seu privado!")
 }
