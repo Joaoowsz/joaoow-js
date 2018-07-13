f390ba1efe68df61ab073ec53af0e71c893fc7a4
João Catozzi <jao_catozzi@hotmail.com>
2018-07-13 16:31:20 -0300

    a

--- commands/send.js
+++ commands/send.js
@@ -3,7 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     let msg = message.content.toLowerCase();
-    if(mentionMessage = null) return message.reply("digite alguma mensagem.")
+    let rMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if(!rMention) return message.reply("digite alguma mensagem.");
 
     if (message.author.bot) return;
     mention = message.mentions.users.first();
