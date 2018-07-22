d3e4a54043ac9a3961a30a25d3cdf4af47f20533
João Catozzi <jao_catozzi@hotmail.com>
2018-07-21 23:03:18 -0300

    k

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Você não possui permissão!");
-    if(!args[0]) return message.channel.send("a");
+    if(!args[0]) return message.channel.send("Defina a quantidade de mensagens que deseja apagar.");
     message.channel.bulkDelete(args[0]).then(() => {
         message.channel.send(`Foram excluídas ${args[0]} mensagens!`).then(msg => msg.delete(100));
     });
