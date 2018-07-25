01b550d82331e0762b5b86687c53ef05840a5bbf
João Catozzi <jao_catozzi@hotmail.com>
2018-07-25 12:37:58 -0300

    a

--- commands/report.js
+++ commands/report.js
@@ -7,6 +7,18 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send({
       "embed": {
         "description": `**Usuário Reportado:** ${rUser}\n**Reportado Por:** ${message.author}\n**Motivo da Denúncia:** ${rreason}`,
+        "url": message.author.displayAvatarURL,
+        "color": 5500275,
+        "timestamp": new Date(),
+        "footer": {
+          "icon_url": message.author.displayAvatarURL,
+          "text": "Enviado por: " + message.author.username
+        },
+        "author": {
+          "name": "Nodus - Denúncias",
+          "url": "https://discordapp.com",
+          "icon_url": "https://cdn.discordapp.com/attachments/470513964300632074/471702548932329482/ulqHkNL.jpg"
+        },
       }
     })
 
