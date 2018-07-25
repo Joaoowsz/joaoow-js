ed663563b4c851eae6b362f1a9ffce03f23a907b
João Catozzi <jao_catozzi@hotmail.com>
2018-07-25 12:49:07 -0300

    a

--- commands/report.js
+++ commands/report.js
@@ -4,28 +4,21 @@ module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Usuário não encontrado.");
     let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("**Denúncias**")
+    .setColor("#36d2f9")
+    .addField("Usuário Reportado:", `${rUser} - **ID:** ${rUser.id}`)
+    .addField("Reportado Por:", `${message.author} - **ID:** ${message.author.id}`)
+    .addField("Canal da Denúncia:", message.channel)
+    .addField("Motivo da Denúncia:", rreason);
+
     let reportschannel = message.guild.channels.find(`name`, "❗denuncias");
     if(!reportschannel) return message.channel.send("Canal de denúncias não encontrado.");
 
 
     message.delete().catch(O_o=>{});
-    reportschannel.send(message.channel.send({
-      "embed": {
-        "description": `**Usuário Reportado:** ${rUser}\n**Reportado Por:** ${message.author}\n**Motivo da Denúncia:** ${rreason}`,
-        "url": message.author.displayAvatarURL,
-        "color": 5500275,
-        "timestamp": new Date(),
-        "footer": {
-          "icon_url": message.author.displayAvatarURL,
-          "text": "Enviado por: " + message.author.username
-        },
-        "author": {
-          "name": "Nodus - Denúncias",
-          "url": "https://discordapp.com",
-          "icon_url": "https://cdn.discordapp.com/attachments/470513964300632074/471702548932329482/ulqHkNL.jpg"
-        },
-      }
-    }));
+    reportschannel.send(reportEmbed);
 
 }
  
