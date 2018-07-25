107d77901ed9ebefe324c9b68f5dc73f6c6f0397
João Catozzi <jao_catozzi@hotmail.com>
2018-07-25 12:46:07 -0300

    a

--- commands/report.js
+++ commands/report.js
@@ -4,7 +4,8 @@ module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Usuário não encontrado.");
     let rreason = args.join(" ").slice(22);
-    let reportEmbed = new message.channel.send({
+    
+    message.channel.send({
       "embed": {
         "description": `**Usuário Reportado:** ${rUser}\n**Reportado Por:** ${message.author}\n**Motivo da Denúncia:** ${rreason}`,
         "url": message.author.displayAvatarURL,
@@ -27,7 +28,23 @@ module.exports.run = async (bot, message, args) => {
 
 
     message.delete().catch(O_o=>{});
-    reportschannel.send(reportEmbed);
+    reportschannel.send(message.channel.send({
+      "embed": {
+        "description": `**Usuário Reportado:** ${rUser}\n**Reportado Por:** ${message.author}\n**Motivo da Denúncia:** ${rreason}`,
+        "url": message.author.displayAvatarURL,
+        "color": 5500275,
+        "timestamp": new Date(),
+        "footer": {
+          "icon_url": message.author.displayAvatarURL,
+          "text": "Enviado por: " + message.author.username
+        },
+        "author": {
+          "name": "Nodus - Denúncias",
+          "url": "https://discordapp.com",
+          "icon_url": "https://cdn.discordapp.com/attachments/470513964300632074/471702548932329482/ulqHkNL.jpg"
+        },
+      }
+    }));
 
 }
  
