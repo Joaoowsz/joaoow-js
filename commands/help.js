f09d0aea6355def0b9a2e7a7717f55dd520a910a
João Catozzi <jao_catozzi@hotmail.com>
2018-07-26 01:38:23 -0300

    h

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-message.channel.send({
+message.author.send({
 "content": "Ajuda - Nodus Bot",
   "embed": {
     "description": "**Prefix = ``!``**",
@@ -81,6 +81,8 @@ message.channel.send({
       }
         ]
   }
+}).catch(err => {
+  message.channel.send("dm privada")
 })
 }
 
