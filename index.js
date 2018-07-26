7da786b395eb4adbb3fd1ebcf08cbf68b51af9f0
João Catozzi <jao_catozzi@hotmail.com>
2018-07-26 01:55:14 -0300

    a

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("futebol", {type: "PLAYING"});
+  bot.user.setActivity("factions no loth.us", {type: "PLAYING"});
 
 });
 
