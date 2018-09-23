29857f5097e16c7c9e0682806638064549aa2a12
João Catozzi <jao_catozzi@hotmail.com>
2018-09-23 20:11:39 -0300

    a

--- commands/ban.js
+++ commands/ban.js
@@ -2,21 +2,26 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return message.channel.send("Usuário não encontrado!");
+    if(!bUser) return message.channel.send("**[US]** User not found.\n**[PT]**Usuário não encontrado.");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Você não possui permissão!");
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Este usuário não pode ser banido!");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**[US]** You do not have permission.\n**[PT]** VOcê não possui permissão.");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**[US]** This user can not be banned!\n**[PT]** Este usuário não pode ser punido.");
 
-    let banEmbed = new Discord.RichEmbed()
-    .setDescription("**Punição - Banimento**")
-    .setColor("#f95336")
-    .addField("Usuário punido:", `${bUser} - **ID:** ${bUser.id}`)
-    .addField("Punido por:", `<@${message.author.id}> - **ID:** ${message.author.id}`)
-    .addField("Canal da punição:", message.channel)
-    .addField("Motivo da punição:", bReason);
+    let banEmbed = new message.channel.send({
+      "embed": {
+        "title": "[PT] Punições - Intel Corporation",
+        "description": `\n**Punição:** Banimento\n**Usuário punido:** ${bUser}\n**Punido por:** ${message.author.name}\n**Motivo:** ${bReason}`,
+        "url": "https://images-ext-1.discordapp.net/external/MZf3GbJLoJUf9uQXPdsG681FX7_TWe-6UBnJPsuKW4s/https/cdn.discordapp.com/attachments/490177937094737941/491616735279972352/latest.png",
+        "color": 1597539,
+        "footer": {
+          "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+          "text": "Equipe de moderação - Intel Corporation"
+        }
+      }
+    })
 
     let punicoeschannel = message.guild.channels.find(`name`, "⛔punicoes");
-    if(!punicoeschannel) return message.channel.send("Canal de punições não encontrado.");
+    if(!punicoeschannel) return message.channel.send("**[US]** Channel of punishments not found.\n**[PT]** Canal de punições não encontrado.");
 
     message.guild.member(bUser).ban(bReason);
     punicoeschannel.send(banEmbed);
