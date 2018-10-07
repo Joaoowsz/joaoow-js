124c3a2bd171cf2bc21f840075ce195a16dc6f75
João Catozzi <jao_catozzi@hotmail.com>
2018-10-07 09:25:40 -0300

    a

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.on("guildMemberAdd", async member => {
   let welcomechannel = member.guild.channels.find(`name`, "🌞bem-vindo");
   welcomechannel.send({
     "embed": {
-      "description": `**IP:** atlanticmc.com.br \n**Twitter:** https://twitter.com/RedeAtlantic \n**Loja:** ~~Em breve~~`,
+      "description": `\n**IP:** atlanticmc.com.br \n**Twitter:** https://twitter.com/RedeAtlantic \n**Loja:** ~~Em breve~~`,
       "url": null,
       "color": 9964474,
       "timestamp": new Date(),
@@ -46,8 +46,8 @@ bot.on("guildMemberAdd", async member => {
         "url": "https://images-ext-1.discordapp.net/external/0vwGKF6HeV7-sSeRwOQjfWzCslPkN8FS9rf-0AJMFT8/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/496949356151439360/2e435a7132130a0cdca32cd29932ed10.png?width=473&height=473"
       },
       "author": {
-        "name":`Olá ${member}, seja bem vindo ao AtlanticMC - Oficial!`,
-        "icon_url": "https://images-ext-1.discordapp.net/external/0vwGKF6HeV7-sSeRwOQjfWzCslPkN8FS9rf-0AJMFT8/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/496949356151439360/2e435a7132130a0cdca32cd29932ed10.png?width=473&height=473"
+        "name":`Olá ${username}, seja bem vindo ao AtlanticMC - Oficial!`,
+        "icon_url": null
       }
     }
   });
