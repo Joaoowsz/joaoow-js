00f6626775d6d61d9198af6c77b3c043a1dc854d
João Catozzi <jao_catozzi@hotmail.com>
2018-10-07 21:43:46 -0300

    a

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return message.channel.send("``❗`` Use: !ban <@username> <motivo!>.");
+    if(!bUser) return message.channel.send("``❗`` Use: !ban <@username> <motivo>.");
     let bReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("``❌`` Você não possui permissão para executar esse comando.");
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("``❌`` Este usuário não pode ser banido.");
