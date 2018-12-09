e84998d52f36177aee4cb64b5aaa70ce08c6a1e1
João Catozzi <jao_catozzi@hotmail.com>
2018-12-09 03:33:48 -0200

    a

--- commands/perguntar.js
+++ commands/perguntar.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     
-    if(!args[1]) return message.reply("Use: !perguntar @FadeMC <pergunta>");
+    if(!args[1]) return message.reply("Use: !perguntar <pergunta>");
     let replies = ["Sim", "Não", "Sei não em..."];
 
     let result = Math.floor((Math.random() * replies.length));
