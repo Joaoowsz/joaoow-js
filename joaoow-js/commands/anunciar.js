50c3896f272ebb7eb44efb38ef298dba04d81c4b
João Catozzi <jao_catozzi@hotmail.com>
2018-12-09 02:10:21 -0200

    a

--- joaoow-js/commands/anunciar.js
+++ joaoow-js/commands/anunciar.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.channel.send("@everyone")
+  message.channel.send("@everyone ")
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Você não possui permissão!");
     let botmessage = args.join(" ");
