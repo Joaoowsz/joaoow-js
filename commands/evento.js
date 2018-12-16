5f26c1882403f0138acdb291eac28a2e540be752
João Catozzi <jao_catozzi@hotmail.com>
2018-12-16 21:11:06 -0200

    a

--- commands/evento.js
+++ commands/evento.js
@@ -4,7 +4,6 @@ module.exports.run = async (bot, message, args) => {
 
 
     if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("``❌`` Você não possui permissão para executar esse comando.");
-    if(!message.member.send) return message.reply ("``❗`` Use: ``!evento [Evento] [Horário] [IP] [Premiação]``\n**Exemplo:** !evento Mini-Fade 20:00 a1.hg.fademc.com.br VIP - 1 Dia")
     let evento = args.slice(1).join(" ");
     let horario = args.slice(2).join(" ");
     let ip = args.slice(3).join(" ");
@@ -15,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
             "description": `**Para participar basta se conectar ao IP informado abaixo**.\n\n**Evento:** ${evento}\n**Horário:** ${horario}\n**IP:** ${ip}\n**Premiação:** ${premiacao}`,
             "url": "https://i.imgur.com/Stenp0u.png",
             "color": 9380674,
-            "timestamp": "2018-12-16T22:03:48.994Z",
+            "timestamp": new Date(),
             "footer": {
               "icon_url": message.author.displayAvatarURL,
               "text": "Responsável: " + message.author.username
