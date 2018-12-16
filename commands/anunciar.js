3ea8d527a99ee7a6d36b4eeefe7a6d34145f4112
João Catozzi <jao_catozzi@hotmail.com>
2018-12-16 10:58:55 -0200

    a

--- commands/anunciar.js
+++ commands/anunciar.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("❌ Você não possui permissão para executar esse comando.");
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("❌ Você não possui permissão para executar esse comando.");
     let botmessage = args.slice(1).join(" ");
     message.delete().catch();
     message.channel.send({
