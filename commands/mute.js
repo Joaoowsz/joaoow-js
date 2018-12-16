8515dff02dba9059cbefd20c56cdbc45dd89d66b
João Catozzi <jao_catozzi@hotmail.com>
2018-12-15 23:22:38 -0200

    a

--- commands/mute.js
+++ commands/mute.js
@@ -9,7 +9,18 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("``❌`` Você não possui permissão para executar esse comando.");
     if(mUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("``❌`` Esse usuário não pode ser mutado!");
 
-    message.channel.send(`${mUser} foi mutado por <@${message.author.id}>.\nMotivo: ${mReason}`)
+    message.channel.send({
+     "embed": {
+      "description": `**${mUser}** foi mutado por **<@${message.author.id}>**\n**Motivo:** ${mReason}`,
+      "url": `https://discordapp.com`,
+      "color": 15105570,
+      "footer": {
+        "icon_url": "https://cdn.discordapp.com/attachments/521157541905039360/521167240247246848/Discord.png",
+        "text": "Equipe de moderação - FadeMC"
+      }
+    }
+  })
+  
 
     var role = message.guild.roles.find(`name`, 'Mutado')
 
