1eddb288e33b3d159bca3ab0418a00a18c1004cc
João Catozzi <jao_catozzi@hotmail.com>
2018-12-15 23:34:33 -0200

    a

--- commands/mute.js
+++ commands/mute.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     if(!mUser) return message.channel.send("``❗`` Use: ``!mute <@username> <motivo>``");
     let mReason = args.join(" ").slice(22);
     let staffrole = message.guild.roles.find(`name`, '☀️ Staff')
-    if(!message.roles.has(staffrole)) return message.channel.send("``❌`` Você não possui permissão para executar esse comando.");
+    if(!message.hasRole(staffrole)) return message.channel.send("``❌`` Você não possui permissão para executar esse comando.");
     if(mUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("``❌`` Esse usuário não pode ser mutado!");
 
     message.channel.send({
