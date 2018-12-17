dd4fe1bb23b26432f5a8732b40ee73c49ce9a3ed
João Catozzi <jao_catozzi@hotmail.com>
2018-12-17 00:00:07 -0200

    a

--- commands/notificar.js
+++ commands/notificar.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-member.addRole("&524015252015153152")
+member.addRole("524015252015153152")
 
 message.channel.send("Você recebeu a tag ``🔔 Notificar``")
 }
