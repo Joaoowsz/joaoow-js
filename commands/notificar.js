fce98ac80ed17ca90165a3806584c16fcfd817c3
João Catozzi <jao_catozzi@hotmail.com>
2018-12-17 16:42:10 -0200

    a

--- commands/notificar.js
+++ commands/notificar.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    var role = guild.roles.find(`name`, '🔔 Notificar');
-    member.addRole(role)
+    var role = guild.roles.find(`name`,'🔔 Notificar');
+    member.addRole(role);
     message.channel.send("Você recebeu a tag ``🔔 Notificar``")
 }
 
