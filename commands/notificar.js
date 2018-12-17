56ab0570c38e42a1546057d4665e78c846759578
João Catozzi <jao_catozzi@hotmail.com>
2018-12-16 23:49:16 -0200

    a

--- commands/notificar.js
+++ commands/notificar.js
@@ -2,9 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    var role = member.guild.roles.find(`name`, '🔔 Notificar')
-     member.addRole(role)
-};
+    var role = member.guild.roles.find(`name`, '🔔 Notificar');
+
+if(member.roles.has(role)) return message.channel.send("``❗`` Você já tem o cargo ``🔔 Notificar``.");
+await(member.addRole(role)); 
+
+message.channel.send("Você recebeu a tag ``🔔 Notificar``");
+}
+
 
     module.exports.help = {
         name:"notificar"
