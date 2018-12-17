c49c0a637a3d9642b4af99b252dae1aa66a87bb5
João Catozzi <jao_catozzi@hotmail.com>
2018-12-16 23:18:58 -0200

    a

--- commands/notificar.js
+++ commands/notificar.js
@@ -2,13 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    let role = guild.roles.find(`name`,'@🔔 Notificar');
-    if(!member.roles.has(role)) return fMmember.removeRole(role) + "Retirei sua tag ``🔔 Notificar``!\nCaso queira novamente basta utilizar ``!notificar``.";
-
-    member.addRole(role)
-    message.channel.send("Agora você tem a tag ``🔔 Notificar``\nCaso queira remover, basta utilizar ``!notificar`` novamente!")
-
-
+    let role = message.guild.roles.find(`name`,'@🔔 Notificar');
+    let member = message.guild.member;
+    if(!member.addRole(role)) return member.removeRole(role) + "Agora você não tem a tag ``🔔 Notificar``."
+    await(member.addRole(role));
+    message.channel.send("Agora você tem a tag ``🔔 Notificar``!\nCaso queira retirar, basta utilizar ``!notificar`` novamente.");
 
     module.exports.help = {
         name:"notificar"
