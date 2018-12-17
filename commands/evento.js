f75dfda1aae72f808c3b99e4b064e762f452480c
João Catozzi <jao_catozzi@hotmail.com>
2018-12-16 22:12:12 -0200

    a

--- commands/evento.js
+++ commands/evento.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
     let ip = args[2];
     let premiacao = args.slice(3).join(" ");
     let canal = message.guild.channels.find(`name`,'🎮eventos');
-    canal.send("@everyone")
+    canal.send("<@&524015252015153152>")
     canal.send({
         "embed": {
             "description": `**Para participar basta se conectar ao IP informado abaixo**.\n\n**Evento:** ${evento}\n**Horário:** ${horario}\n**IP:** ${ip}\n**Premiação:** ${premiacao}`,
