85932cd8907e62c16577159bbdc51378b4f2bb28
João Catozzi <jao_catozzi@hotmail.com>
2018-12-18 16:33:51 -0200

    g

--- commands/notificar.js
+++ commands/notificar.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+    if(message.channel.id != "523838496691978240" && message.channel.id != "523586087696990208") return message.channel.send(" ");
     let role = message.guild.roles.find(`name`, '🔔 Notificar');
     let member = message.member;
     if(!member.addRole(role));
