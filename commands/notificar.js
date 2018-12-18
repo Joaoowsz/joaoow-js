016871030b1e420b9c6d0b819ca2a1d0274990a7
João Catozzi <jao_catozzi@hotmail.com>
2018-12-18 02:14:40 -0200

    a

--- commands/notificar.js
+++ commands/notificar.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    let role = message.guild.roles.find(`name`,'🔔 Notificar');
-    let member = message.author;
-    if(!role) return message.channel.send('Você não será mais notificado dos eventos.')
-    if(!member.roles.has(role)) return member.removeRole(role) && message.channel.send("Agora você será notificado de todos os eventos.");
+    let role = message.guild.roles.find(`name`, '🔔 Notificar');
+    let member = message.member;
+    if(!member.roles.has(role)) return member.removeRole(role) && message.channel.send(" agora você será notificado de todos os eventos.");
+    if(!member.role.has(role)) return member.addRole(role) && message.channel.send(" agora você não irá mais receber notificações dos eventos.");
        
 }
     module.exports.help = {
