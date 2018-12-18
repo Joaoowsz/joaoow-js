571eddec0b4b173ecf28bae6e90812757a3f6726
João Catozzi <jao_catozzi@hotmail.com>
2018-12-17 23:34:27 -0200

    a

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.on('guildMemberAdd', member => {
 bot.on('message', async message => {
 
   if(message.channel.id != "523838496691978240","523586087696990208") return message.channel.send(" ");
-})
+});
 
 bot.on('message', async message => {
   let member = message.member;
