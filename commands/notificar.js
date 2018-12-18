a72b96993c27ca77d079f1991c16ebf7b96abc6d
João Catozzi <jao_catozzi@hotmail.com>
2018-12-18 01:41:48 -0200

    a

--- commands/notificar.js
+++ commands/notificar.js
@@ -4,10 +4,8 @@ module.exports.run = async (bot, message, args) => {
 
     let role = message.guild.roles.find(`name`, '🔔 Notificar');
     let member = message.member;
-    if(!member.has.roles(role)) return member.removeRole(role)
-    message.channel.send(" agora você será notificado de todos os eventos.");
-    member.addRole(role)
-    message.channel.send(" agora você não irá mais receber notificações dos eventos.");
+    if(!member.has.roles(role)) return member.removeRole(role) && message.channel.send(" agora você será notificado de todos os eventos.");
+    member.addRole(role) && message.channel.send(" agora você não irá mais receber notificações dos eventos.");
 
 }
     module.exports.help = {
