842b8614c51f582753a063c32503146908189964
João Catozzi <jao_catozzi@hotmail.com>
2018-12-18 22:48:51 -0200

    s

--- commands/ascii.js
+++ commands/ascii.js
@@ -2,12 +2,12 @@ const ascii = require('ascii-art');
 
 module.exports.run = async (bot, message, args) => {
 
-    ascii.font(args.join(" "), 'Doom', function(rendered) {
+    ascii.font(args.join(' '), 'Doom', function(rendered) {
 
         rendered = rendered.trimRight();
-        if (rendered.length > 2000) return message.channel.send("Essa mensagem é grande demais!");
+        if(rendered.length > 2000) return message.channel.send("Essa mensagem é grande demais!");
         message.channel.send(rendered, {
-            code: "md"
+            code: "nd"
         });
     });
 
