89855eaf9cefa44dc0e81375b1ab47391d6ad11d
João Catozzi <jao_catozzi@hotmail.com>
2018-12-18 23:00:56 -0200

    a

---
+++ commands/unmute.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    let mUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let role = message.guild.roles.find(`name`, 'Mutado');
+    if(!mUser.roles.has(role)) return message.channel.send("❌ | Esse usuário não está mutado.");
+    mUser.removeRole(role) && message.channel.send(`✔ | O usuário ${mUser} foi desmutado por ${message.author.username}.`);
+
+    };
+    module.exports.help = {
+        name: "unmute"
+    }
