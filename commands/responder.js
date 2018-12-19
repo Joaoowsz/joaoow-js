3375443e6bd9cae3927cfaac3f6f778974a7efa4
João Catozzi <jao_catozzi@hotmail.com>
2018-12-19 13:33:32 -0200

    aa

--- commands/responder.js
+++ commands/responder.js
@@ -5,16 +5,17 @@ module.exports.run = async (bot, message, args) => {
     let member = message.member;
     let tUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if (!tUser) return message.channel.send("❗ | Utilize: ``!responder <@username> <resposta>``");
-    let tResposta = args.slice(1).join(" ");
+    let tResposta = args.slice(2).join(" ");
     if(!tResposta) return message.channel.send(`O usuário ${tUser} foi respondido com sucesso.`);
     
     const embed = new Discord.RichEmbed()
   .setAuthor(`Seu ticket foi respondido!`, message.author.displayAvatarURL)
-  .addField('Ticket:', `**Autor:** ${tUser.tag}\n**Dúvida:** ${args}\n**Resposta: ${tResposta}\n**Staffer: ${message.author.usernampe}\n**Status:** Fechado`)
+  .addField('Ticket:', `**Autor:** ${tUser.username}\n**Dúvida:** ${args}\n**Resposta: ${tResposta}\n**Staffer: ${message.author.username}\n**Status:** Fechado`)
   .setThumbnail("https://i.imgur.com/Stenp0u.png")
   .setColor("#ffd700");
 
     tUser.send(embed);
+    message.channel.send(`O usuário ${tUser} foi respondido com sucesso.`);
 
 
 }
