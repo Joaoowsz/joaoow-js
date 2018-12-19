87432d39415c80e0aead9de702ad2bda44668954
João Catozzi <jao_catozzi@hotmail.com>
2018-12-19 13:49:09 -0200

    a

--- commands/responder.js
+++ commands/responder.js
@@ -2,21 +2,33 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    let member = message.member;
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("❌ | Você não possui permissão para executar esse comando.");
     let tUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if (!tUser) return message.channel.send("❗ | Utilize: ``!responder <@username> <resposta>``");
-    let tResposta = args.slice(1).join(" ");
-    if(!tResposta) return message.channel.send(`O usuário ${tUser} foi respondido com sucesso.`);
-    
-    const embed = new Discord.RichEmbed()
-  .setAuthor(`Seu ticket foi respondido!`, message.author.displayAvatarURL)
-  .addField('Ticket:', `**Autor:** ${tUser.username}\n**Dúvida:** ${args}\n**Resposta: ${tResposta}\n**Staffer: ${message.author.username}\n**Status:** Fechado`)
-  .setThumbnail("https://i.imgur.com/Stenp0u.png")
-  .setColor("#ffd700");
-
-    tUser.send(embed);
-    message.channel.send(`O usuário ${tUser} foi respondido com sucesso.`);
-
+    let tMessage = args.slice(1).join(" ");
+    message.delete().catch();
+    message.channel.send("✔ | O Ticket de " + tUser.username + " foi respondido com sucesso!")
+    tUser.send({
+  "embed": {
+    "description": "**Autor:** " + tUser.username + "\n**Resposta:** " + "\n**Staffer:** " + message.author.username + "\n**Status:** Fechado",
+    "url": null,
+    "color": 1752220,
+    "timestamp": new Date(),
+    "footer": {
+      "icon_url": message.author.displayAvatarURL,
+      "text": "Atenciosamente, " + message.author.username
+    },
+    "thumbnail": {
+      "url": tUser.displayAvatarURL
+    }, 
+    "author": {
+      "name": "Seu ticket foi respondido!",
+      "url": null,
+      "icon_url": "https://i.imgur.com/Stenp0u.png"
+    }
+  }
+    })
 
 }
     module.exports.help = {
