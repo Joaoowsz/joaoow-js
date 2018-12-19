7ad23d416b037cea417cc14483fd0d8793260c8f
João Catozzi <jao_catozzi@hotmail.com>
2018-12-19 16:10:25 -0200

    a

--- commands/ticketmessage.js
+++ commands/ticketmessage.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     let ticket = message.guild.channel.find(`name`, '🎐ticket');
-    let embed = {
+    ticket.send({
         "embed": {
             "description": "**\nComo funciona?**\nBasta digitar !ticket <sua dúvida> no <#523838496691978240>.",
             "color": 3131281,
@@ -19,11 +19,10 @@ module.exports.run = async (bot, message, args) => {
               "name": "Sistema de tickets - FadeMC",
               "url": "https://discordapp.com",
               "icon_url": "https://i.imgur.com/Stenp0u.png"
-            }      
-              
+            }
             
-          }
-    }
+        }
+    })
 }
 module.exports.help = {
     name: "ticketmessage"
