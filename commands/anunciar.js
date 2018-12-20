5d9d916bed88768a8c5de6471b85d2c7ff038552
João Catozzi <jao_catozzi@hotmail.com>
2018-12-20 16:34:24 -0200

    a

--- commands/anunciar.js
+++ commands/anunciar.js
@@ -29,33 +29,11 @@ module.exports.run = async (bot, message, args) => {
     await msg1.react('📸');
     await msg1.react('📝');
 
+    if(react.collected('📸')) return message.channel.send('Use: ``!anunciar <link da imagem> <mensagem>')
 
 
-    if(member.react('📸')) return message.channel.send(`Use: !anunciar <link da imagem> <mensagem do anúncio>`)
 
 
-    canal.send("@everyone")
-    canal.send({
-  "embed": {
-    "description": botmessage,
-    "url": null,
-    "color": 1752220,
-    "timestamp": new Date(),
-    "footer": {
-      "icon_url": message.author.displayAvatarURL,
-      "text": "Atenciosamente, " + message.author.username
-    },
-    "image": {
-      "url": imageurl
-    }, 
-    "author": {
-      "name": " Rede Fade informa:",
-      "url": null,
-      "icon_url": "https://images-ext-1.discordapp.net/external/YFjo9kmtQ0r3WM1KGn-ga2k9g5i6obaYK-f9KK9z5SU/%3Fv%3D1/https/cdn.discordapp.com/emojis/480512404229193736.gif"
-    }
-  }
-    })
-  }
 
 module.exports.help = {
     name: "anunciar"
