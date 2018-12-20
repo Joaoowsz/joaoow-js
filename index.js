deb6ac0e7ea049c8154a25f980df928510fe97f0
João Catozzi <jao_catozzi@hotmail.com>
2018-12-20 14:57:04 -0200

    a

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ bot.on('guildMemberRemove', member => {
   var mutado = member.guild.roles.find(`name`, 'Mutado')
 
   if(!member.roles.has(mutado)) return;
-  await bot.channels.get("523683124308213781").send(`Um usuário **mutado** saiu do servidor.\n**Usuário:** ${member.user.username}\n**ID:** ${member.id}`)
+  await bot.channels.get("523683124308213781").send(`Um usuário **mutado** saiu do servidor.\n**Usuário:** ${member}\n**ID:** ${member.id}`)
 })
 
 
