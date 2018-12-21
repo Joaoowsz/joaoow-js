a01a9f9cc19d208c40f8ad401673fc496e5f5a61
João Catozzi <jao_catozzi@hotmail.com>
2018-12-21 13:11:00 -0200

    k

--- commands/evento.js
+++ commands/evento.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(message.channel.id != "523586087696990208") return message.channel.send(" ");
-  let promotor = bot.roles.get("522912997983911936");
+  let promotor = message.guild.roles.find("🚀 Promotor de Eventos ");
     if(!message.roles.has(promotor)) return message.reply("``❌`` Você não possui permissão para executar esse comando.");
     if(!args[0]) return message.channel.send("``❗`` Use: ``!evento [evento] [horário] [IP] [Premiação]``\nExemplo: ``!evento Mini-Fade 20:00 a1.hg.fademc.com.br VIP - 2 DIAS``")
     let evento = args[0];
