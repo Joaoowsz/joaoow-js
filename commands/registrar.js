51f27b27e0547bb3a498487af8fbc4d1fe3c0389
João Catozzi <jao_catozzi@hotmail.com>
2018-12-26 16:16:20 -0200

    a

--- commands/registrar.js
+++ commands/registrar.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
    
-    if(message.channel.id != "523838496691978240" && message.channel.id != "523586087696990208") return message.channel.send(" ");
+    if(message.channel.id != "525708464740106261" && message.channel.id != "523586087696990208") return message.channel.send(" ");
     let role = message.guild.roles.find(`name`, 'Registrando');
     let cargo = message.guild.roles.find(`name`, 'Membro');
     let member = message.member;
